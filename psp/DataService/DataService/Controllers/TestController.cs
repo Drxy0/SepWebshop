040c1793253093309c64e0dfa0aee1c4af0b55ef
Acile067 <acile067@gmail.com>
2026-01-05 13:05:59 +0100

    dockerfile

---
+++ psp/DataService/DataService/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DataService.Controllers
+{
+    [Route("d/test")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("ping")]
+        public IActionResult Ping()
+        {
+            return Ok("Pong DataService");
+        }
+    }
+}
