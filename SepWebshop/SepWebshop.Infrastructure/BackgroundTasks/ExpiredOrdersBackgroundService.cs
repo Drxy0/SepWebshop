1d8cb584fe065b9b596628c88943eed8293325d5
Acile067 <acile067@gmail.com>
2026-01-08 21:15:02 +0100

    try catch

--- SepWebshop/SepWebshop.Infrastructure/BackgroundTasks/ExpiredOrdersBackgroundService.cs
+++ SepWebshop/SepWebshop.Infrastructure/BackgroundTasks/ExpiredOrdersBackgroundService.cs
@@ -1,21 +1,37 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 using SepWebshop.Application.Orders.Services;
 
 namespace SepWebshop.Infrastructure.BackgroundTasks;
-
-public class ExpiredOrdersBackgroundService(IServiceScopeFactory scopeFactory) : BackgroundService
+public class ExpiredOrdersBackgroundService(
+    IServiceScopeFactory scopeFactory,
+    ILogger<ExpiredOrdersBackgroundService> logger) : BackgroundService
 {
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
+        await Task.Delay(TimeSpan.FromMinutes(3), stoppingToken);
+        logger.LogInformation("Expired Orders Background Service is starting.");
+
         while (!stoppingToken.IsCancellationRequested)
         {
-            using var scope = scopeFactory.CreateScope();
-            CancelExpiredOrdersService cancelService = scope.ServiceProvider.GetRequiredService<CancelExpiredOrdersService>();
+            try
+            {
+                using var scope = scopeFactory.CreateScope();
+                var cancelService = scope.ServiceProvider.GetRequiredService<CancelExpiredOrdersService>();
 
-            await cancelService.CancelUnpaidOrdersAsync(stoppingToken);
+                logger.LogInformation("Running task: Canceling expired unpaid orders...");
 
-            await Task.Delay(TimeSpan.FromMinutes(1), stoppingToken);
+                await cancelService.CancelUnpaidOrdersAsync(stoppingToken);
+
+                logger.LogInformation("Successfully processed expired orders.");
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occurred while processing expired orders.");
+            }
+
+            await Task.Delay(TimeSpan.FromMinutes(3), stoppingToken);
         }
     }
 }
