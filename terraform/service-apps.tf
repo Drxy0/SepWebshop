c17ddaa3b00ba64b783f67f42a7a830f51d3f400
Acile067 <acile067@gmail.com>
2026-01-26 19:07:14 +0100

    infra improvement

--- terraform/service-apps.tf
+++ terraform/service-apps.tf
@@ -74,3 +74,49 @@ resource azurerm_linux_web_app psp_frontend {
     }
   }
 }
+
+resource azurerm_linux_web_app bank_frontend {
+  name                = "lwa-${var.application_name}-fe-bank-${var.environment_name}-${var.location_short}-${var.resource_version}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  service_plan_id     = azurerm_service_plan.main.id
+
+  site_config {
+    ip_restriction_default_action = "Allow"
+    minimum_tls_version           = 1.2
+    always_on                     = true
+    app_command_line              = "npx serve -s ."
+    application_stack {
+      node_version = "22-lts"
+    }
+  }
+}
+
+resource azurerm_linux_web_app bank_backend {
+  name                = "lwa-${var.application_name}-be-bank-${var.environment_name}-${var.location_short}-${var.resource_version}"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+  service_plan_id     = azurerm_service_plan.main.id
+
+  site_config {
+    ip_restriction_default_action = "Allow"
+    minimum_tls_version           = 1.2
+    always_on                     = true
+
+    application_stack {
+      dotnet_version = "8.0"
+    }
+}
+
+  identity {
+    type = "SystemAssigned"  
+  }
+  connection_string {
+    name  = "DefaultConnection"
+    type  = "SQLAzure"
+    value = "Data Source=sql-${var.application_name}-${var.environment_name}-${var.location_short}-${var.resource_version}.database.windows.net,1433;Initial Catalog=sqldb-${var.application_name}-bankdb-${var.environment_name}-${var.location_short}-${var.resource_version};Persist Security Info=False;User ID=${data.azurerm_key_vault_secret.admin_login.value};Password=${data.azurerm_key_vault_secret.admin_password.value};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+  }
+  app_settings = {
+
+  }
+}
